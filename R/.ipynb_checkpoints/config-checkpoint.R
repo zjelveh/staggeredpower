09392633cf68940069ae81501ec5b55bef346220
Zubin Jelveh <zjelveh@umd.edu>
2025-01-09 09:55:49 -0500

    Delete R/.ipynb_checkpoints directory
    remove ipynb checkpoints

--- R/.ipynb_checkpoints/config-checkpoint.R
+++
@@ -1,8 +0,0 @@
-# R/config.R
-#' Load Power Analysis Configuration
-#' 
-#' @param config_path Path to YAML config file
-#' @export
-load_config <- function(config_path) {
-  yaml::read_yaml(config_path)
-}
